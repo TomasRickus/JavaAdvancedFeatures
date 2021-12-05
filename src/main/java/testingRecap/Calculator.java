0882e957d9876ce3474effcd6088bcb753fbf27e
tomri <tomrickus@hmail.com>
2021-12-05 20:06:55 +0200

    Added logger and some methods in calculator

--- src/main/java/testingRecap/Calculator.java
+++ src/main/java/testingRecap/Calculator.java
@@ -2,6 +2,8 @@ package testingRecap;
 
 import org.apache.log4j.Logger;
 
+import java.util.Arrays;
+
 public class Calculator {
 
     private static final Logger LOGGER = Logger.getLogger(Calculator.class);
@@ -12,6 +14,7 @@ public class Calculator {
     }
 
     public int addMultipleNumbers(int[] numbers) {
+        LOGGER.info("Adding numbers " + Arrays.toString(numbers));
         int total = 0;
         for (int number : numbers) {
             total += number;
@@ -20,4 +23,41 @@ public class Calculator {
         return total;
     }
 
+    public int subtractionTwoNumbers(int firstNumber, int secondNumber) {
+        LOGGER.info("Subtracting two numbers " + firstNumber + " and " + secondNumber);
+        return firstNumber - secondNumber;
+    }
+
+    public int subtractionMultipleNumbers(int[] numbers) {
+        LOGGER.info("Subtracting numbers " + Arrays.toString(numbers));
+        int total = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            total -= numbers[i];
+        }
+
+
+        return total;
+
+    }
+
+    public int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public int multiplyMultipleNumbers(int[] numbers) {
+        int total = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            total *= numbers[i];
+
+        }
+        return total;
+
+    }
+
+    public double divideIntegers(int firstNumber, int secondNumber) {
+        return (double) firstNumber / secondNumber;
+    }
+
 }
+
+
--- src/test/java/testingRecap/CalculatorTest.java
+++ src/test/java/testingRecap/CalculatorTest.java
@@ -2,13 +2,104 @@ package testingRecap;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
 
+    private Calculator calculator = new Calculator();
+
     @Test
-    void add() {
+    void testAddTwoNumbers() {
+        //given
+        int firstNumber = 12;
+        int secondNumber = 3;
+
+        //when
+        int actualAddResult = calculator.add(firstNumber, secondNumber);
+
+        //then
+        assertEquals(15, actualAddResult); //JUNIT assertEquals
+        assertThat(actualAddResult).isEqualTo(15); //tas pats su ASSERTJ biblioteka
     }
 
     @Test
-    void addMultipleNumbers() {
+    void testAddMultipleNumbers() {
+
+        int[] numbersToTest = {5, 2, -6, 8, 1};
+
+        int actualResult = calculator.addMultipleNumbers(numbersToTest);
+
+        assertEquals(10, actualResult);
+        assertThat(actualResult).isEqualTo(10);
+    }
+
+    @Test
+    void testSubtractTwoNumbers() {
+
+        int firstNumber = 10;
+        int secondNumber = 5;
+
+        int actualResult = calculator.subtractionTwoNumbers(firstNumber, secondNumber);
+
+        assertThat(actualResult).isEqualTo(5);
+    }
+
+    @Test
+    void testSubtractionMultipleNumbers() {
+
+        int[] numbersToTest = {5, 8, 3, 2, 1};
+
+        int actualResult = calculator.subtractionMultipleNumbers(numbersToTest);
+
+        assertThat(actualResult).isEqualTo(-9);
+    }
+
+    @Test
+    void testMultiplyTwoNumbers() {
+
+        int firstNumber = 5;
+        int secondNumber = 6;
+
+        int actualResult = calculator.multiply(firstNumber, secondNumber);
+
+        assertEquals(30, actualResult);
+    }
+
+    @Test
+    void testMultiplyMultipleNumbers() {
+
+        int[] numberToTest = {3, 5, 2};
+
+        int actualResult = calculator.multiplyMultipleNumbers(numberToTest);
+
+        assertThat(actualResult).isEqualTo(30);
+
+    }
+
+    @Test
+    void testDivideTwoNumbers() {
+        int firstNumber = 4;
+        int secondNumber = 2;
+
+        double actualResult = calculator.divideIntegers(firstNumber, secondNumber);
+
+        assertThat(actualResult).isEqualTo(2);
+    }
+
+    @Test
+    void testDivideTwoNumbersWhereResultIsNotAnInteger() {
+        int firstNumber = 9;
+        int secondNumber = 2;
+
+        double actualResult = calculator.divideIntegers(firstNumber, secondNumber);
+
+        assertThat(actualResult).isEqualTo(4.5);
+
+    }
+
+    @Test()
+    void testDivideNumberByZero () {
+
     }
 }
